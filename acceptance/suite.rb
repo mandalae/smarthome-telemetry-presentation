87f5d03778680d0fc3c7522621523b1e5b418ec4
Chris Skaaning <c@rpediem.com>
2018-10-29 12:27:16 +0000

    Added Mark and changed a few things in the slides, plus added stage5 of the pipeline

--- acceptance/suite.rb
+++ acceptance/suite.rb
@@ -19,6 +19,6 @@ end
 
 run do
   @driver.get 'http://skaaning.ninja'
-  expect(@driver.title).to eql "Panoptic culture"
+  expect(@driver.title).to eql "Panoptic culture - this is the wrong title"
   # @driver.save_screenshot('docker_image.png')
 end
